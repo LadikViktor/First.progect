35b85fe4f7218079204b92ea9d68a44d103d4793
LadikViktor <59064966+LadikViktor@users.noreply.github.com>
2019-12-24 20:58:54 +0300

    next php

--- index.php
+++ index.php
@@ -11,7 +11,33 @@
 
 <?php
 
-echo "Hello World";
+
+$a=5;
+$c=$a<<1;
+echo $c;
+echo "<br>";
+
+$bool = TRUE; // логический
+$str = "foo"; // строковый 
+$int = 12; // целочисленный
+
+echo gettype($bool); // выводит "boolean"
+echo gettype($str); // выводит "string"
+
+// Если это целое, увеличить на четыре
+if(is_int($int)); {
+    $int +=4;
+}
+
+// Если $bool - это строка, вывести ее
+//(ничего не выводит)
+if(is_string($bool)){
+    echo "Строка: $bool";
+}
+
+
+
+
 
 ?>
 </body>
