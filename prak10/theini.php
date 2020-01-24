f8df8374ab604b2f6d00959847ee48196a08c643
LadikViktor <59064966+LadikViktor@users.noreply.github.com>
2020-01-24 21:26:28 +0300

    praktika10

--- prak10/theini.php
+++ prak10/theini.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,24 +14,24 @@
             display: flex;
             justify-content: center;
             margin-top: 100px;
-            font-family: Century;
-
-
-
-
+            
         }
     </style>
 </head>
-
 <body>
     <?php
-    file_put_contents("file.txt", file_get_contents("http://php.net"));
+//Обработка без секций
+$ini_array = parse_ini_file("file.ini");
+echo "<pre>";
+print_r($ini_array);
+echo "<br>";
+//Обработка с секциями
+echo "<pre>";
+$ini_array = parse_ini_file("file.ini", true);
+print_r($ini_array);
+
 
 
     ?>
-
-
-
 </body>
-
 </html>
--- probachet.php
+++ probachet.php
@@ -12,11 +12,14 @@
 
     <?php
 
-    $f = file_get_contents('filename_1.txt');
-    echo '<span style="color:green; font-size: 40px;">' . ($f+=1) . '</span>';
+    // $f = file_get_contents('filename_1.txt');
+    // echo '<span style="color:green; font-size: 40px;">' . ($f+=1) . '</span>';
+    // file_put_contents('filename_1.txt', $f);
+
+    echo $f = file_get_contents('filename_1.txt')+1;
     file_put_contents('filename_1.txt', $f);
 
-
+   
 
 
     //     $f = ("filename_1.txt");
@@ -27,13 +30,13 @@
     // fclose($fp);
     // echo $h[0];
 
-        $handle = fopen("http://php.net/", "r");
-        $contents = '';
-        while (!feof($handle)) {
-            $contents .= fread($handle, 1);
-        }
-        fclose($handle);
-        echo $contents;
+    //     $handle = fopen("http://php.net/", "r");
+    //     $contents = '';
+    //     while (!feof($handle)) {
+    //         $contents .= fread($handle, 1);
+    //     }
+    //     fclose($handle);
+    //     echo $contents;
 
 
 
