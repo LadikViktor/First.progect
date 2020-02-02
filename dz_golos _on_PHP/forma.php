649f4c5be673c003b3677210c45826a9e878a608
LadikViktor <59064966+LadikViktor@users.noreply.github.com>
2020-02-02 17:20:41 +0300

    opros result

---
+++ dz_golos _on_PHP/forma.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .form {
+            display: flex;
+            flex-direction: column;
+            background-color: #DCDCDC;
+            font-size: 15px;
+            border: 2px solid black;
+            border-radius: 10px;
+            width: 300px;
+            height: 250px;
+            margin-left: 10px;
+            margin-top: 10px;
+        }
+
+        h3 {
+            display: flex;
+            justify-content: center;
+          margin-left: 10px;
+           
+        }
+
+        label {
+            justify-content: left;
+            display: flex;
+            background-color: white;
+            margin-left: 10px;
+            margin-right: 10px;
+
+
+
+        }
+
+        .chek {
+            margin-top: 20px;
+            background-color: blue;
+            width: 50px;
+            height: 25px;
+            margin-left: 10px;
+            border: 1px solid #BEBEBE;
+            border-radius: 10px;
+
+        }
+
+        .stroka {
+            background-color: white;
+            width: 250px;
+            height: 20px;
+            margin-top: 10px;
+            margin-left: 10px;
+            margin-right: 10px;
+            border: 1px solid #BEBEBE;
+            border-top: none;
+            border-left: none;
+            border-right: none;
+            font-family: Arial, Helvetica, sans-serif;
+
+        }
+
+        .pole {
+            background-color: white;
+            height: 100px;
+            margin-left: 10px;
+            margin-right: 10px;
+            border: 1px solid #BEBEBE;
+            border-radius: 10px;
+
+
+        }
+    </style>
+</head>
+
+<body>
+    <div class='form'>
+        <form action="voit.php" method="POST">
+            <?php
+            $arr = file('swoi.txt');
+            ?>
+            <h3><?= $arr[0] ?></h3>
+            <div class="pole">
+                <?php
+
+                for ($i = 1; $i < count($arr); $i++) {
+                    $buf = explode(" - ", $arr[$i])[0];
+                    echo "<div class='stroka'><label><input type='radio' name='voting' value='$i'>$buf</label><br></div>";
+                }
+
+                ?>
+            </div>
+
+            <input class='chek' type="submit" value="OK">
+
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ dz_golos _on_PHP/voit.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .result {
+            display: flex;
+            flex-direction: column-reverse;
+            font-size: 20px;
+            border: 2px solid gray;
+            border-radius: 10px;
+            justify-content: center;
+            width: 250px;
+            height: 250px;
+            margin: 10px;
+            background-color: white;
+            padding-left: 10px;
+
+        }
+
+        h3 {
+            display: flex;
+            justify-content: center;
+            padding-left: 10px;
+            
+          
+        }
+
+        .fonresult{
+            display: flex;
+            flex-direction: column;
+            background-color: #DCDCDC;
+            border: 2px solid black;
+            border-radius: 10px;
+            width: 300px;
+            height: 250px;
+            margin-left: 10px;
+            margin-top: 10px;
+            padding-left: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    $ind = $_POST['voting'];
+    $arr = file('swoi.txt');
+    $buf = explode(" - ", $arr[$ind]);
+    $buf[1] += 1;
+    $arr[$ind] = implode(" - ", $buf) . "\n";
+    file_put_contents('swoi.txt', $arr);
+
+    $sum = 0;
+    $array = array(1, 2);
+    for ($i = 1; $i < count($arr); $i++) {
+        $sum = $sum + explode(" - ", $arr[$i])[1];
+    }
+
+
+
+    ?>
+     <div class = "fonresult">
+    <h3><?= $arr[0] ?></h3>
+   
+    <div class="result">
+        <?
+        for ($i = 1; $i < count($arr); $i++) {
+            $buf = explode(' - ', $arr[$i]);
+
+            echo   $buf[0] . " - " . round($buf[1] / $sum * 100) . "%";
+            echo "  <div style = 'height: 20px; border: 1px solid;border-radius: 5px; width:" . (100 * $buf[1] / $sum) . "px; background-color: red; display: flex; flex-direction: column; margin: 5px;'></div> ";
+        }
+
+        ?>
+
+    </div>
+    </div>
+</body>
+
+</html>
