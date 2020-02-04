da3acf5345c4accd2d976c1de0865f5c57a64e8b
LadikViktor <ksuxa2016atp>
2020-02-04 21:29:12 +0300

    dsdsds

---
+++ praktika12 04.02.2020/chat/ekran.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="refresh" content="1">
+    <title>Document</title>
+    <link href="main.css" rel='stylesheet'>
+</head>
+<body>
+    <?php
+    $mes_arr = file("script.txt");
+foreach($mes_arr as $key => $value){
+    
+    // echo "<div class = 'odd'>$value </div>";
+    echo "<div class = '" .(($key % 2)?'odd':'even'). "'>$value </div>";
+}
+    ?>
+</body>
+</html>
---
+++ praktika12 04.02.2020/chat/forma.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    
+</head>
+
+<body>
+    <!-- <meta HTTP-equiv= "REFRECH" content -->
+    <form action='?' method='POST'>
+        <input type = 'text' value="<?=$_POST ['UserName']?>" name ="UserName">
+        <input type = 'text' name="for">
+      
+ 
+        <input class="submit" type="submit" value="OK">
+    </form>
+
+    <?php
+if(!empty($_POST['for'])){
+file_put_contents("script.txt", $_POST['UserName']. ":". $_POST['for']. "\n", FILE_APPEND);
+
+}
+
+
+
+
+    ?>
+</body>
+
+</html>
