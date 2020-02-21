1650d7595243f162611d4a2ee4b526c5872b7961
LadikViktor <ksuxa2016atp>
2020-02-21 21:06:50 +0300

    rasmer file

--- praktira16 18.02.2020/File to server copy/refr.php
+++ praktira16 18.02.2020/File to server copy/refr.php
@@ -15,11 +15,12 @@ for ($dir; $name1 = readdir($dir); $dir++) {
                     $icon = 's.jpg';
                     break;
             }
-            echo "<a href='file/$name1'><img src= '$icon' width='16' height='16'> $name1 </a>
+            $ras = round((filesize("file/$name1") / 1024) / 1024, 3);
+            echo "<a href='file/$name1'><img src= '$icon' width='16' height='16'> $name1 </a> $ras MB
             <a href='del.php?file=$name1'><img src='kis.jpg''width='30' height='30'><br><br>";
         } else {
 
-            echo "<a href='file/$name1'><img src= 'im.jpg'width='80' height='80'>$name1</a>
+            echo "<a href='file/$name1'><img src= 'im.jpg'width='80' height='80'>$name1</a> $ras MB
             <a href='derect.php?file=$name1'><img src='kis.jpg''width='30' height='30'><br><br>";
         }
     }
